1d2805c6f0d6d941b277d3562c407f1b4f53b9cb
ChasingRain <junpeng94@gmail.com>
2022-08-29 01:22:50 +0800

    added table header logic and mock data

--- src/components/layout/Tab.js
+++ src/components/layout/Tab.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Tab() {
-  return (
-    <div>Tab</div>
-  )
+    const activeTab = () => {
+
+    }
+    return (
+    <div class="tabs">
+        <Link to='/' className="tab tab-lifted">Home</Link> 
+        <Link to='/' className="tab tab-lifted tab-active"> Leave Entitlement </Link> 
+    </div>
+    )
 }
 
 export default Tab
--- src/components/layout/Table.js
+++ src/components/layout/Table.js
@@ -1,9 +1,39 @@
 import React from 'react'
 
-function Table() {
-  return (
-    <div>Table</div>
-  )
+function Table({headerType}) {
+
+    const requestTableHeader = ["Leave Type", "Period", "No. of calendar days", "Submitted on", "Quota used", "Status", "Action" ]
+    const historyTableHeader = ["Leave Type", "Period", "No. of calendar days", "Submitted on", "Quota used", "Status" ]
+    const EntitlementTableHeader = ["Leave Type", "Validity", "Entitlement", "Quota used", "Available"]
+
+    let header = (headerType === "entitlement") ? EntitlementTableHeader 
+                                                        : (headerType === "request") ? requestTableHeader 
+                                                        : historyTableHeader
+
+
+    const mockEntitlementData = [
+        ["Vacation Leave", "05.05.2022 - 04.02.2023", "1 days", "1 days", "0 days"],
+        ["Medical Leave", "05.05.2022 - 31.12.2022", "1 days", "10 days", "0 days"],
+    ]
+
+    return (
+    <div class="overflow-x-auto">
+        <table class="table w-full">
+            {/* <!-- head --> */}
+            <thead>
+            <tr>
+                {header.map(headerName => <th>{headerName}</th>)}
+            </tr>
+            </thead>
+            <tbody>
+            {mockEntitlementData.map(list =>            
+            <tr>
+                {list.map(listItem => <th>{listItem}</th>)}
+            </tr>)}
+            </tbody>
+        </table>
+    </div>
+    )
 }
 
 export default Table
