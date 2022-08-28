69c85d7f5dc8cf840cd2d0cbaf36cbde17c49171
ChasingRain <junpeng94@gmail.com>
2022-08-28 23:30:27 +0800

    setting up of tailwind and daisyui, added homepage, tab and table component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
+import Homepage from "./pages/Homepage";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route exact path = '/' element={<Homepage/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
