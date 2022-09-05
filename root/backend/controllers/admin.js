791481f6ae04cdfcf2bd0a5dd4e9cccc35339d08
ChasingRain <junpeng94@gmail.com>
2022-09-05 17:14:46 +0800

    hashed password during user creation

--- root/backend/controllers/admin.js
+++ root/backend/controllers/admin.js
@@ -1,11 +1,12 @@
 const User = require('../models/user')
 
+const bcrypt = require('bcryptjs')
+
 exports.postCreateUser = (req,res,next) => {
     const name = req.body.name
     const isAdmin = req.body.isAdmin
     const email = req.body.email
     const password = req.body.password
-    const confirmPassword = req.body.confirmPassword
     const createdOn = req.body.createdOn
     const lastUpdatedOn = req.body.lastUpdatedOn
     const ro = req.body.ro
@@ -14,30 +15,46 @@ exports.postCreateUser = (req,res,next) => {
     const coveringEmail = req.body.coveringEmail
     const leaveLeft = req.body.leaveLeft
     
-    const user = new User({
-        name: name,
-        isAdmin: isAdmin,
-        email: email,
-        password: password,
-        confirmPassword: confirmPassword,
-        createdOn: createdOn,
-        lastUpdatedOn: lastUpdatedOn,
-        ro: ro,
-        reportingEmail: reportingEmail,
-        co: co,
-        coveringEmail: coveringEmail,
-        leaveLeft: leaveLeft
-    })
-
-    user
-        .save()
-        .then(result => {
-            // console.log(result)
-            console.log("user created")
+    User.findOne({email: email})
+        .then(userDoc => {
+            if(userDoc){
+                console.log(userDoc)
+                return res.status(499).send("email already exist")
+                // return res.redirect('http://localhost:3000/create-user')
+            }
+            return bcrypt
+                .hash(password, 12)
+                .then(hashedPassword => {
+                    const user = new User({
+                        name: name,
+                        isAdmin: isAdmin,
+                        email: email,
+                        password: hashedPassword,
+                        createdOn: createdOn,
+                        lastUpdatedOn: lastUpdatedOn,
+                        ro: ro,
+                        reportingEmail: reportingEmail,
+                        co: co,
+                        coveringEmail: coveringEmail,
+                        leaveLeft: leaveLeft
+                    })
+                
+                    user
+                    .save()
+                    .then(result => {
+                        // console.log(result)
+                        console.log("user created")
+                    })
+                    .catch(err => {
+                        console.log(err)
+                    })
+                    res.status(200).send(user)   
+        
+                })
         })
         .catch(err => {
             console.log(err)
         })
 
-    res.status(200).send(user)   
+
 }
--- root/backend/models/user.js
+++ root/backend/models/user.js
@@ -19,10 +19,6 @@ const userSchema = new Schema({
         type: String,
         required: true
     },
-    confirmPassword: {
-        type: String,
-        required: true
-    },
     createdOn: {
         type: String,
         required: true
